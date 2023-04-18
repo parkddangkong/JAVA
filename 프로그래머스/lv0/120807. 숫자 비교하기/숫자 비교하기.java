3cfc4722b9f3427a4668511c5b62d12e3ff03fd5
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-04-18 17:56:09 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean val = (0<=num1&&num1<=100&&0<=num2&&num2<=100);
+        int answer = 0;
+        
+        if(num1==num2){
+            answer = 1;
+        }
+        else{
+            answer = -1;
+        }
+        return answer;
+    }
+}
