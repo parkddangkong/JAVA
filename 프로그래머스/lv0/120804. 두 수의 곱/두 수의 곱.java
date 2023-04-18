5dc52a1a53b8d02089b9f8bd511d80e5c5552ecd
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-04-18 17:53:54 +0900

    [level 0] Title: 두 수의 곱, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean val = (0<=num1&&num1<=100&&0<=num2&&num2<=100);
+        int answer = num1*num2;
+        return answer;
+    }
+}
