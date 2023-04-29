38f667902abe5f047ea28b6e560027abf75f24dc
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-04-29 22:12:40 +0900

    [unrated] Title: 문자열로 변환, Time: 1.11 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        answer = String.format("%d", n);
+        // String.format으로 n을 문자열로 만들어줌
+        return answer;
+    }
+}
