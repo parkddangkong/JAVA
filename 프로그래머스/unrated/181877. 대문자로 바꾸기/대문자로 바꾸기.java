a6a426f52005d98314c34289c70b3873860aa3bd
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-04-29 21:52:54 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toUpperCase();
+        // String =  Java의 내장 클래스이며, 문자열을 저장하고 조작하는 데 사용
+        // answer = 변수명
+        // myString.toUpperCase = myString에 있는 스펠링을 대문자로 바꿔줌
+        return answer;
+        // answer을 return함
+    }
+}
