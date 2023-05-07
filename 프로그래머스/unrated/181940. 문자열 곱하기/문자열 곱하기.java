2d1b78e30c8838d2ed2805dded9668cd299e7799
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-05-07 16:32:31 +0900

    [unrated] Title: 문자열 곱하기, Time: 2.17 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for(int i=0;i<k;i++){   // k번 반복하도록 for문 조건식
+            answer += my_string;    // answer 안에 mt_string 문자열이 반복하도록함
+    }
+         return answer;
+    }
+}
