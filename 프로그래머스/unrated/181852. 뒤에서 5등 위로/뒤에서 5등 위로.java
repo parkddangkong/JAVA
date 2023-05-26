7a259ffe0ef0f17e756c018c4be64a9c8e2ef5e1
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-05-26 16:28:27 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.51 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length-5];
+        Arrays.sort(num_list);
+        int num = 0;
+        for(int i = 5 ;i<num_list.length;i++) {
+        	answer[num] = num_list[i];
+        	num++;
+        }
+        return answer;
+    }
+}
