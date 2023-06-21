98e535259bfe460b4f8909ff19d168df9bd4a5f6
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-06-21 17:07:51 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int angle) {
-        int answer =0;
-        if(0<angle&&angle<90){
-           answer=1;
-        }
-        else if(angle==90){
-            answer= 2;
-        }
-        else if(90<angle&&angle<180){
-            answer=3;
-        }
-        else{
-            answer=4;
+        int answer = 0;
+            if(angle > 0 && angle < 90){
+                answer = 1;
+            }
+            if(angle == 90){
+                answer = 2;
+            }
+            if(angle > 90 && angle < 180){
+                answer = 3;
+            }
+            if(angle == 180){
+                answer = 4;
         }
         return answer;
     }
