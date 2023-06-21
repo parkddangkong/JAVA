b6c9fe1566fc08a0a23eda8fd9d64ce65182c927
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-06-21 16:19:02 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
-        boolean val= (-50000<=num1&&num1<=50000&&-50000<=num2&&num2<=50000);
-        int answer = num1-num2;
-        
+        int answer = 0;
+        answer = num1 - num2;
         return answer;
     }
 }
