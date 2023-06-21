2def0fea60c23faf83e5b06c69f7e814cedcb6f9
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-06-21 17:48:24 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,16 +1,7 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer;
-        int drink;
-        
-        answer = n*12000;
-        drink = k*2000;
-        
-        if(n>=10){
-            drink -= (n/10)*2000; 
-        }
-        
-        
-        return answer+ drink;
+        int answer = 0;
+        answer = n * 12000 + ((k - (n / 10))*2000);
+        return answer;
     }
 }
