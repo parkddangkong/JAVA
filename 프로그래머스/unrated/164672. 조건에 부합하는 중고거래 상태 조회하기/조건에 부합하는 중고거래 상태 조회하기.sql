1e8c2137361bc49ee2bd5c9577aa97467489dfa1
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-08-27 21:00:31 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,17 @@
+-- 코드를 입력하세요
+SELECT
+
+        BOARD_ID
+       ,WRITER_ID
+       ,TITLE
+       ,PRICE
+       ,CASE WHEN STATUS = 'SALE' THEN '판매중'
+             WHEN STATUS = 'RESERVED' THEN '예약중'
+             WHEN STATUS = 'DONE' THEN '거래완료' 
+             ELSE 'Unknown'
+         END AS STATUS
+
+FROM USED_GOODS_BOARD
+WHERE 1=1
+AND TO_CHAR(CREATED_DATE,'YYYY-MM-DD') = '2022-10-05'
+ORDER BY BOARD_ID DESC
