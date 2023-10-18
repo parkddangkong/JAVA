e80c8e27f96f60504791d35966fe8ff57e7b262f
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2023-10-18 23:09:48 +0900

    [level 1] Title: 최소직사각형, Time: 13.45 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,19 @@
+function solution(sizes) {
+    let answer = 0;
+    for (let arr of sizes) {
+        arr.sort((a, b) => b - a);;
+    }
+
+    let front = [], back = [];
+    sizes.forEach(i => {
+        front.push(i[0]);
+        back.push(i[1]);
+    });
+
+    front.sort((a,b) => b - a);
+    back.sort((a,b) => b - a);
+
+    answer = front[0] * back[0];
+
+    return answer;
+}
