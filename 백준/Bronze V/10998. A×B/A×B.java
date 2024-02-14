0072a1964e10a507b9283ddd6244294ca9da167e
parkddangkong <130978806+parkddangkong@users.noreply.github.com>
2024-02-14 21:13:22 +0900

    [Bronze V] Title: A×B, Time: 112 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze V/10998. A×B/A×B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	
+				Scanner Sc = new Scanner(System.in);
+				
+				int A = Sc.nextInt();
+				int B = Sc.nextInt();
+				
+				int answer = A*B;
+				
+				System.out.print(answer);
+	
+	}
+
+}
